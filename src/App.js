4a5a8f416b51ec3893e95f60d55f2b32e8b095b9
Vicenç Sallés <vicesalles@protonmail.com>
2019-03-26 16:18:28 +0100

    fix: Theme

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import {Switch, Route, withRouter} from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
+//Estil Material
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 //Vistes
 import Home from './views/Home';
 import Rector from './views/Rector';
 
+const theme = createMuiTheme({
+    palette:{
+      primary:{
+        main:"#40ae33"
+      }
+    }
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">     
          
-        
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/r/:id/:secs?" component={Rector}/>
-        </Switch>
-      
+        <MuiThemeProvider theme={theme}>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/r/:id/:secs?" component={Rector}/>
+          </Switch>
+        </MuiThemeProvider>
       </div>
     );
   }
