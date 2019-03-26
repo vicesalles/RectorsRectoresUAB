cf31da71ff74d1c47feade28ac6d91df5d544db0
Vicenç Sallés <vicesalles@protonmail.com>
2019-03-26 12:50:33 +0100

    feat: capcalera material-ui

--- src/components/Reproductor/Capcalera.js
+++ src/components/Reproductor/Capcalera.js
@@ -1,12 +1,81 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import HomeIcon from '@material-ui/icons/Home';
+
+
+const styles = {
+    root: {
+      flexGrow: 1,
+    },
+    cap:{
+        marginLeft:25
+    },
+    menuButton: {
+      marginLeft: -18,
+      marginRight: 10,
+    },
+    bigAvatar: {
+        margin: 10,
+        width: 60,
+        height: 60,
+      },
+      wikiBu: {        
+        width: 20,
+        height: 20,
+      }
+  };
+
 
 class Capcalera extends Component{
+
+
     render(){
-        return(<div>
-                <h1 className="nom_rector">{this.props.currentRector ? `${this.props.currentRector.nom} ${this.props.currentRector.cognoms}`:"Nom del rector"}</h1>
-              </div>)
+        
+        const { classes } = this.props;
+
+        if(this.props.currentRector){
+
+            return( <div>
+                <AppBar position="static">
+                    <Toolbar variant="dense">   
+
+                            <IconButton href="/" target="_blank">
+                                <HomeIcon className={classes.bigAvatar}/>             
+                            </IconButton>
+                   
+                            <Avatar alt={this.props.currentRector.cognoms} src={`/img/l/${this.props.currentRector.url}.jpg`} className={classes.bigAvatar}/>
+                            
+                            <Grid direction="column" justify="flex-start" alignItems="flex-start" alignContent="flex-start" className={classes.cap}>
+                                <Grid container direction="row" justify="flex-start">
+                                    <Typography variant="h4" color="inherit" align="left">
+                                        {`${this.props.currentRector.nom} ${this.props.currentRector.cognoms}`}
+                                    </Typography>   
+                                    <IconButton size="small" href={this.props.currentRector.vikipedia} target="_blank">
+                                        <Avatar alt="Wikipedia" src={`/img/bu/wiki.png`} className={classes.wikiBu}/>                 
+                                    </IconButton>
+                                </Grid> 
+                                <Typography variant="h7" color="inherit" align="left">
+                                    {this.props.currentRector.mandat}
+                                </Typography>
+                            </Grid>      
+                    </Toolbar>
+                </AppBar>               
+               
+          </div>)
+
+        }else{
+            return(<div>Carregant...</div>)
+        }
+
+       
     }
 }
 
@@ -18,4 +87,4 @@ function mapStateToProps(state){
 
 }
 
-export default withRouter(connect(mapStateToProps)(Capcalera));
+export default withRouter(connect(mapStateToProps)(withStyles(styles)(Capcalera)));
