b9fc9701b318e99c86f1797dfadddda2dac8b248
Vicenç Sallés <vicesalles@protonmail.com>
2019-05-21 12:21:56 +0200

    fix: Cleaned warns

--- src/components/Home/CartaRector.js
+++ src/components/Home/CartaRector.js
@@ -1,11 +1,7 @@
 import React,{Component} from 'react';
 import {withRouter} from 'react-router-dom';
-import {connect} from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
