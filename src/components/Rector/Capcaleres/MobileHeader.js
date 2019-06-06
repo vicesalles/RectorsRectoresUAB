d9ff2236cd2be8fba973fe9a46b9c969d39874c4
Vicenç Sallés <vicesalles@protonmail.com>
2019-06-06 12:18:49 +0200

    style: Mobile header

--- src/components/Rector/Capcaleres/MobileHeader.js
+++ src/components/Rector/Capcaleres/MobileHeader.js
@@ -1,49 +1,19 @@
 import React,{Component} from 'react';
 import {withRouter} from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
+//Material UI components
+import {AppBar,Toolbar,Typography,IconButton,Avatar,Grid} from '@material-ui/core';
+//Icon
 import HomeIcon from '@material-ui/icons/Home';
 
-//Estil
-
-const styles = {
-    root: {
-      flexGrow: 1,
-    },
-    cap:{
-        marginLeft:25
-    },
-    menuButton: {
-      marginLeft: -18,
-      marginRight: 10,
-    },
-    smallAvatar: {
-        margin: 5,
-        width: 30,
-        height: 30,
-      },
-      wikiBu: {        
-        width: 15,
-        height: 15,
-      },
-      mandat:{
-          fontFamily:"Roboto"
-      }
-  };
-
-  class MobileHeader extends Component {
+class MobileHeader extends Component {
 
     render(){
          //Classes
          const { classes } = this.props;
 
          return(<AppBar position="fixed">
-         <Toolbar variant="dense">  
+         <Toolbar variant="dense" className={classes.barraMobil}>  
                  <IconButton href="/">
                      <HomeIcon className={classes.smallAvatar}/>             
                  </IconButton>             
@@ -63,5 +33,36 @@ const styles = {
     }
 
   }
+
+  //Estil
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  cap:{
+      marginLeft:15,
+      color:"white"
+  },
+  menuButton: {
+    marginLeft: -18,
+    marginRight: 10,
+  },
+  smallAvatar: {       
+      width: 30,
+      height: 30,
+    },
+    wikiBu: {        
+      width: 15,
+      height: 15,
+    },
+    mandat:{
+        fontFamily:"Roboto"
+    },
+    barraMobil:{
+      marginLeft:0,
+      paddingLeft:0
+    }
+};
 
   export default withRouter(withStyles(styles)(MobileHeader));
