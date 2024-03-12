984d5fe9162b7f38c86e2221ce1fc99a91fcebea
vicesalles <vicenc.salles@gmail.com>
2024-03-12 10:37:57 +0100

    fix: eliminat el serviceworker

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './state/store';
 import {HashRouter} from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<Provider store={store}><HashRouter><App /></HashRouter></Provider>, document.getElementById('root'));
 
 
-serviceWorker.register();
+
