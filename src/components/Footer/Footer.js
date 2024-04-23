165a8dada7527fbf887c4ffb74a5c4d27d72fea0
vicesalles <vicenc.salles@gmail.com>
2024-04-23 11:36:51 +0200

    chore: Sitemap i navegació alternativa

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 function Footer(){
-    return(<div id="peu"> 
+    return(<div id="peu">            
             <div>                
-                <Link to="/credits"><img alt="Universitat Autònoma de Barcelona 50è Aniversari" src="./img/logos/logotip-uab-versio-3-verd.png"></img>  </Link>    
+            <div style={{flex:1,flexDirection:"row"}}>
+             <Link to="/r/serra/"><p>- Antoni Serra</p></Link>
+             <Link to="/r/pascual/"><p>- Ramon Pascual</p></Link>
+             <Link to="/r/valles/"><p>- Josep M. Vallès</p></Link>
+             <Link to="/r/sola/"><p>- Carles Solà</p></Link>
+             <Link to="/r/ferrer/"><p>- Lluís Ferrer</p></Link>
+             <Link to="/r/ripoll/"><p>- Ana Ripoll</p></Link>
+             <Link to="/r/sancho/"><p>- Ferran Sancho</p></Link>
+             <Link to="/r/arboix/"><p>- Margarita Arboix -</p></Link>
+            </div>
+                <Link to="/credits"><img alt="Universitat Autònoma de Barcelona 50è Aniversari" src="./img/logos/logotip-uab-versio-3-verd.png"></img>  </Link> 
+                <a style={{'color':'gray'}} href="./sitemap.xml"><p>Mapa del lloc web (Sitemap)</p></a>
             </div>              
     </div>)
 }
